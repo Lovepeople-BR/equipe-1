b5af60681e74248fb8419e1d689d3599ec797222
Luccas Bikelis <luccasbikelis@gmail.com>
2021-06-28 20:22:44 -0300

    Tela cadastro concluida

---
+++ lib/view/cadastro/cadastro.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import 'package:dotted_line/dotted_line.dart';
+
+class Cadastro extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFA901F7),
+      body: Center(
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 80),
+                  child: Text(
+                    'Vamos começar!',
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontFamily: "Montserrat-SemiBold"),
+                  ),
+                ),
+                Form(
+                    key: _formKey,
+                    child: Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 40,
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10)),
+                                  ),
+                                  hintText: "Nome",
+                                  hintStyle:
+                                      TextStyle(color: Color(0xFF3101B9)),
+                                  fillColor: Colors.white,
+                                  filled: true,
+                                  contentPadding: EdgeInsets.only(left: 15)),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            height: 40,
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(10))),
+                                  hintText: "Número de telefone, email ou CPF",
+                                  hintStyle:
+                                      TextStyle(color: Color(0xFF3101B9)),
+                                  fillColor: Colors.white,
+                                  filled: true,
+                                  contentPadding: EdgeInsets.only(left: 15)),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            height: 40,
+                            child: TextFormField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                  suffixIcon: Icon(Icons.remove_red_eye),
+                                  border: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10)),
+                                  ),
+                                  hintText: "Senha",
+                                  hintStyle:
+                                      TextStyle(color: Color(0xFF3101B9)),
+                                  fillColor: Colors.white,
+                                  filled: true,
+                                  contentPadding: EdgeInsets.only(left: 15)),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            height: 40,
+                            child: TextFormField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                  suffixIcon: Icon(Icons.remove_red_eye),
+                                  border: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10)),
+                                  ),
+                                  hintText: "Confirmar senha",
+                                  hintStyle:
+                                      TextStyle(color: Color(0xFF3101B9)),
+                                  fillColor: Colors.white,
+                                  filled: true,
+                                  contentPadding: EdgeInsets.only(left: 15)),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 35,
+                          ),
+                          ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Color(0xFF3101B9)),
+                              shape: MaterialStateProperty.all(
+                                  RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                side: BorderSide(color: Colors.white, width: 2),
+                              )),
+                            ),
+                            onPressed: () {},
+                            child: Container(
+                              width: 100,
+                              child: Center(
+                                child: Text(
+                                  "Cadastrar",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: "Montserrat-SemiBold"),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
+                Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: DottedLine(
+                    direction: Axis.horizontal,
+                    lineLength: double.infinity,
+                    lineThickness: 1.0,
+                    dashLength: 4.0,
+                    dashColor: Colors.white,
+                    dashRadius: 0.0,
+                    dashGapLength: 4.0,
+                    dashGapColor: Colors.transparent,
+                    dashGapRadius: 0.0,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Já possui cadastro? ',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      InkWell(
+                        child: Text(
+                          "Entrar",
+                          style: TextStyle(color: Colors.yellow),
+                        ),
+                        onTap: () {},
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
