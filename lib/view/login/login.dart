efda2979a6085286cad51a7db63211cb62f39ceb
Luccas Bikelis <luccasbikelis@gmail.com>
2021-06-30 17:09:06 -0300

    Atualizações tela login

--- lib/view/login/login.dart
+++ lib/view/login/login.dart
@@ -78,7 +78,7 @@ class _LoginState extends State<Login> {
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(15),
                     child: Container(
                       width: 300,
                       height: 40,
