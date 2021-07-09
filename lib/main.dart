eb4f3c2a31000efc7aa871eab88bebcb43518cca
Luccas Bikelis <luccasbikelis@gmail.com>
2021-07-09 13:26:51 -0300

    Atualizações

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 import 'package:app_lovepeople/globals.dart' as globals;
+import 'package:app_lovepeople/model/repository/delete_repository.dart';
 import 'package:app_lovepeople/model/repository/todo_repository.dart';
 import 'package:app_lovepeople/model/repository/user_repository.dart';
 import 'package:app_lovepeople/presenter/cadastro_controller.dart';
+import 'package:app_lovepeople/presenter/home_controller.dart';
 import 'package:app_lovepeople/presenter/login_controller.dart';
 import 'package:app_lovepeople/presenter/new_todo_controller.dart';
-
 
 import 'package:app_lovepeople/view/login/login.dart';
 import 'package:flutter/cupertino.dart';
@@ -26,36 +27,34 @@ void main() {
 class MyApp extends StatelessWidget {
   static const URL_BASE_API = 'https://todo-lovepeople.herokuapp.com';
   final preferences = LocalPreferences();
- 
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-       Provider.value(value: TodoRepository(URL_BASE_API, preferences)),
+        Provider.value(value: TodoRepository(URL_BASE_API, preferences)),
         Provider.value(value: UserRepository(URL_BASE_API, preferences)),
       ],
-        child:MultiProvider(
-          providers: [
-            ChangeNotifierProvider(
-                create: (context) => NewTodoController(context.read())),
-            ChangeNotifierProvider(
-                create: (context) => CadastroController(context.read())),
-            ChangeNotifierProvider(
-                create: (context) => LoginController(context.read()))
-          ],
-          child: MaterialApp(
-            title: 'Flutter Demo',
-            theme: ThemeData(
-              brightness: Brightness.light,
-              primarySwatch: Colors
-                  .purple, // or Colors.green or any color darker than white (this is line 66)
-              accentColor: Colors.black,
-            ),
-            home: Login(),
+      child: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+              create: (context) => NewTodoController(context.read())),
+          ChangeNotifierProvider(
+              create: (context) => CadastroController(context.read())),
+          ChangeNotifierProvider(
+              create: (context) => LoginController(context.read())),
+        ],
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            brightness: Brightness.light,
+            primarySwatch: Colors
+                .purple, // or Colors.green or any color darker than white (this is line 66)
+            accentColor: Colors.black,
           ),
+          home: Login(),
         ),
-      
+      ),
     );
   }
 }
--- lib/model/repository/todo_repository.dart
+++ lib/model/repository/todo_repository.dart
@@ -1,4 +1,5 @@
 import 'package:app_lovepeople/core/local_preferences.dart';
+import 'package:flutter/cupertino.dart';
 
 import '../todo.dart';
 import 'dart:convert';
@@ -9,21 +10,38 @@ class TodoRepository {
   final LocalPreferences _localPreferences;
   final String baseUrl;
 
-  TodoRepository(this.baseUrl,this._localPreferences,);
+  TodoRepository(
+    this.baseUrl,
+    this._localPreferences,
+  );
 
-  Future<List<Todo>> getTodoList() async {
-    var response = await http.get(
-        Uri.parse('$baseUrl/auth/local'),
-        headers: header);
+  Future<List<Todo>> getTasks(String title, {BuildContext? context}) async {
+    var response = await http.get(Uri.parse('$baseUrl/todos'), headers: header);
     if (response.statusCode == 200) {
       var json = JsonDecoder().convert(response.body);
 
-      return json.map<Todo>((item) {
+      return json.map<dynamic>((item) {
         return Todo.fromJson(item);
       }).toList();
     } else {
       return [];
     }
+  }
+
+  Future<Todo?> deleteItem(int? id) async {
+    await http
+        .delete(
+      Uri.parse('$baseUrl/todos/ /todos/{idTodo}'),
+      headers: header,
+    )
+        .then((value) async {
+      if (value.statusCode == 200) {
+        var json = JsonDecoder().convert(value.body);
+        return Todo.fromJson(json);
+      } else {
+        return null;
+      }
+    });
   }
 
   Future<Todo?> register(Todo todo) async {
@@ -32,8 +50,7 @@ class TodoRepository {
       'Authorization': 'Bearer ${login?.jwt}',
     };
     return http
-        .post(Uri.parse('$baseUrl/todos'),
-            headers: header, body: todo.toJson())
+        .post(Uri.parse('$baseUrl/todos'), headers: header, body: todo.toJson())
         .then((value) async {
       if (value.statusCode == 200) {
         final json = jsonDecode(value.body);
